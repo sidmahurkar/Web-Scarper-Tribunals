bbe432f017962b735fa7c077a732e4c9aa5c19e7
Siddhant Mahurkar <45026019+sidmahurkar@users.noreply.github.com>
2020-02-21 16:02:31 +0530

    Extracts all the data in lists format

--- satgov.py
+++ satgov.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
-# import datetime
+import datetime
 import time
 import os
 
